05e0b08818f7b723d1d88b9f645afa0b2a418563
jcarlosorte <jcarlosorte@ugr.es>
2025-04-14 23:57:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
-// vite.config.js - contenido de ejemplo
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // importante para que funcione bien el enrutamiento relativo en local
+})
