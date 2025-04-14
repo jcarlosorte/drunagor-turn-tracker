f504c5717e82b130c5f8f05a8a9e44db6a2dece1
jcarlosorte <jcarlosorte@ugr.es>
2025-04-15 00:06:15 +0200

    Create Tracker.jsx

---
+++ Tracker.jsx
@@ -0,0 +1,8 @@
+export default function Tracker() {
+  return (
+    <div className="p-4 text-white">
+      <h2 className="text-2xl font-bold">Tracker de Combate</h2>
+      <p>Aquí irá el sistema de turnos.</p>
+    </div>
+  )
+}
