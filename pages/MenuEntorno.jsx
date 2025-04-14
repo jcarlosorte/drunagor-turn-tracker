deeae027d7b27b361e8c34c6b47f1944b2065e66
jcarlosorte <jcarlosorte@ugr.es>
2025-04-15 00:08:41 +0200

    Create MenuEntorno.jsx

---
+++ pages/MenuEntorno.jsx
@@ -0,0 +1,8 @@
+export default function MenuEntorno() {
+  return (
+    <div className="p-4 text-white">
+      <h2 className="text-2xl font-bold">Configuración del Entorno</h2>
+      <p>Aquí se podrá seleccionar idioma, expansiones, etc.</p>
+    </div>
+  )
+}
