84178ea7388f086472410f6a573ab0ce2dc92b54
jcarlosorte <jcarlosorte@ugr.es>
2025-04-16 00:34:11 +0200

    Update Tracker.jsx

--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -1,1 +1,10 @@
-// Página del Tracker
+// src/pages/Tracker.jsx
+const Tracker = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Tracker en construcción</h1>
+    </div>
+  );
+};
+
+export default Tracker;
