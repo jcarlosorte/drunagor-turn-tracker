9d6ab424290ef4597bdc2f8a42d24dcb0e259362
jcarlosorte <jcarlosorte@ugr.es>
2025-04-16 00:50:13 +0200

    Update Config.jsx

--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -1,1 +1,9 @@
-// Página de Configuración
+const Config = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold">Configuración del Entorno</h2>
+    </div>
+  );
+};
+
+export default Config;// Página de Configuración
