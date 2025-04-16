c4f0e787b6da8a7d5571d6fc9471fdad5b888bad
jcarlosorte <jcarlosorte@ugr.es>
2025-04-17 01:32:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { LanguageProvider } from './context/LanguageContext'
-import { ExpansionsProvider } from './context/ExpansionContext'
+import LanguageProvider from './context/LanguageContext'
+import ExpansionsProvider from './context/ExpansionContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
