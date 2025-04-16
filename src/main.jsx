a60aeeaadf46d1bff5995a3fc8c35f0f0450fdcf
jcarlosorte <jcarlosorte@ugr.es>
2025-04-17 00:41:03 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import { LanguageProvider } from './context/LanguageContext'
+import { ExpansionsProvider } from './context/ExpansionsContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <LanguageProvider>
+      <ExpansionsProvider>
+        <App />
+      </ExpansionsProvider>
+    </LanguageProvider>
+  </React.StrictMode>,
+)
