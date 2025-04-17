6b777c52c2bf76ce2f5281b8a0a13e130eb48f64
jcarlosorte <jcarlosorte@ugr.es>
2025-04-17 15:35:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Tracker from "./pages/Tracker.jsx";
 import Config from './pages/Config.jsx';
 
 function Home() {
   return (
     <div>
-      <img src="/drunagor-turn-tracker/logo.png" alt="Drunagor Logo" className="mx-auto w-40 mb-4" />
+      <img src={`${import.meta.env.BASE_URL}logo.png`} alt="Drunagor Logo" className="mx-auto w-40 mb-4" />
       <h1 className="text-4xl font-bold mb-6">Drunagor Turn Tracker</h1>
       <Link
         to="/tracker"
@@ -24,14 +25,23 @@ function Home() {
 }
 
 function App() {
+  const navigate = useNavigate();
   return (
     <Router basename="/drunagor-turn-tracker">
       <div className="text-center mt-10">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tracker" element={<Tracker />} />
-          <Route path="/config" element={<Config />} />
-        </Routes>
+        <button
+  onClick={() => navigate('/tracker')}
+  className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+>
+  Iniciar Tracker
+</button>
+
+<button
+  onClick={() => navigate('/config')}
+  className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-2"
+>
+  Configurar Entorno
+</button>
       </div>
     </Router>
   );
