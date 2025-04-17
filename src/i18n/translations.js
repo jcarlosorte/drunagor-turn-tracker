bdc651a648b4788d641bc317441bc204a315dd84
jcarlosorte <jcarlosorte@ugr.es>
2025-04-18 01:16:32 +0200

    Update translations.js

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -1,12 +1,14 @@
 export const translations = {
   es: {
     config_title: "Configuración",
+    language_section_title: "Idioma de la Aplicación",
     select_language: "Selecciona el idioma",
     select_expansions: "Selecciona las expansiones",
     language_name: "Español"
   },
   en: {
     config_title: "Settings",
+    language_section_title: "App Language",
     select_language: "Select language",
     select_expansions: "Select expansions",
     language_name: "English"
