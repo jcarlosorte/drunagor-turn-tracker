7444ee5b051238c488f036994eba58d3c9647d49
jcarlosorte <jcarlosorte@ugr.es>
2025-04-18 00:56:24 +0200

    Create main2.jsx

---
+++ src/main2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import { LanguageProvider } from './context/LanguageContext';
+import { ExpansionProvider } from './context/ExpansionContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LanguageProvider>
+      <ExpansionsProvider>
+        <App />
+      </ExpansionsProvider>
+    </LanguageProvider>
+  </React.StrictMode>,
+)
