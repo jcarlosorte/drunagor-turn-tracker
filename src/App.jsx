20e4382f8faa9cb1b82eddff6d69cb1f9d18e67c
jcarlosorte <jcarlosorte@ugr.es>
2025-04-18 11:39:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import { useLanguage } from "@/context/LanguageContext";
 import Tracker from "./pages/Tracker.jsx";
 import Config from './pages/Config.jsx';
 
+function Header() {
+  const { translations } = useLanguage();
+  return (
+    <>
+      <img
+        src={`${import.meta.env.BASE_URL}logo.png`}
+        alt="Drunagor Logo"
+        className="mx-auto w-40 mb-4"
+      />
+      <h1 className="text-4xl font-bold mb-6">
+        {translations.home.title}
+      </h1>
+    </>
+  );
+}
+
 function HomeMenu() {
   const navigate = useNavigate();
   const { translations } = useLanguage();
@@ -26,19 +42,10 @@ function HomeMenu() {
 }
 
 function App() {
-  const { translations } = useLanguage();
-
   return (
     <Router basename="/drunagor-turn-tracker/">
       <div className="text-center mt-10">
-        <img
-          src={`${import.meta.env.BASE_URL}logo.png`}
-          alt="Drunagor Logo"
-          className="mx-auto w-40 mb-4"
-        />
-        <h1 className="text-4xl font-bold mb-6">
-          {translations.home.title}
-        </h1>
+        <Header />
 
         <Routes>
           <Route path="/" element={<HomeMenu />} />
