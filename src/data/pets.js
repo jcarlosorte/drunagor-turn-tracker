33fa558ef26432e5b0ef4818a5c75c0aab00ae58
jcarlosorte <jcarlosorte@ugr.es>
2025-04-18 22:24:06 +0200

    Create pets.js

---
+++ src/data/pets.js
@@ -0,0 +1,8 @@
+// pets.js
+export const PETS = [
+  { id: "bliss", nombre: "Bliss", vida: 0, movimiento: 0, color: "light", tipo: "mascota", expansionId: "hero_pack_1" },
+  { id: "gloomy", nombre: "Gloomy", vida: 0, movimiento: 0, color: "shadow", tipo: "mascota", expansionId: "hero_pack_1" },
+  { id: "spark", nombre: "Spark", vida: 0, movimiento: 0, color: "fire", tipo: "mascota", expansionId: "hero_pack_1" },
+  { id: "wisper", nombre: "Wisper", vida: 0, movimiento: 0, color: "air", tipo: "mascota", expansionId: "hero_pack_1" },
+  { id: "mossy", nombre: "Mossy", vida: 0, movimiento: 0, color: "nature", tipo: "mascota", expansionId: "hero_pack_1" }
+];
