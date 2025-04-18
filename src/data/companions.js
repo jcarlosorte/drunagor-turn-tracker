e379de18998780829dc04385dd0820fa48b1ca83
jcarlosorte <jcarlosorte@ugr.es>
2025-04-18 22:23:45 +0200

    Create companions.js

---
+++ src/data/companions.js
@@ -0,0 +1,9 @@
+// companions.js
+export const COMPANIONS = [
+  { id: "alyra", nombre: "Alyra", vida: 0, movimiento: 0, color: "light", tipo: "compañero", expansionId: "hero_pack_1" },
+  { id: "koragg", nombre: "Koragg", vida: 0, movimiento: 0, color: "dark", tipo: "compañero", expansionId: "hero_pack_1" },
+  { id: "sir_thorend", nombre: "Sir Thorend", vida: 0, movimiento: 0, color: "light", tipo: "compañero", expansionId: "hero_pack_1" },
+  { id: "jorgen", nombre: "Jorgen", vida: 0, movimiento: 0, color: "nature", tipo: "compañero", expansionId: "hero_pack_1" },
+  { id: "nereida", nombre: "Nereida", vida: 0, movimiento: 0, color: "water", tipo: "compañero", expansionId: "hero_pack_1" },
+  { id: "renz", nombre: "Renz", vida: 0, movimiento: 0, color: "shadow", tipo: "compañero", expansionId: "hero_pack_1" }
+];
