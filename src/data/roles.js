bf933a6c27e7ba54dcf89a1e1c234737b192fd5d
jcarlosorte <jcarlosorte@ugr.es>
2025-04-19 02:31:52 +0200

    Create roles.js

---
+++ src/data/roles.js
@@ -0,0 +1,25 @@
+// src/data/roles.js
+const roles = [
+  {
+    id: 'tank',
+    expansionId: 'base',
+  },
+  {
+    id: 'support',
+    expansionId: 'base',
+  },
+  {
+    id: 'dps',
+    expansionId: 'base',
+  },
+  {
+    id: 'healer',
+    expansionId: 'base',
+  },
+  {
+    id: 'control',
+    expansionId: 'base',
+  },
+];
+
+export default roles;
