38c409a617dfcb20dd5d652cde26bf556b08070b
jcarlosorte <jcarlosorte@ugr.es>
2025-04-19 22:20:15 +0200

    Create languageData.js

---
+++ src/i18n/languageData.js
@@ -0,0 +1,12 @@
+import es from './es.json';
+import en from './en.json';
+
+export const languages = {
+  es,
+  en,
+};
+
+export const languageNames = {
+  es: es.language_name,
+  en: en.language_name,
+};
