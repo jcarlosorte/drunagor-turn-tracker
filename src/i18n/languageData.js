a8cf970d74b12988ffce24947e32461c78b7a746
jcarlosorte <jcarlosorte@ugr.es>
2025-04-19 23:54:53 +0200

    Update languageData.js

--- src/i18n/languageData.js
+++ src/i18n/languageData.js
@@ -1,10 +1,7 @@
 import es from './es.json';
 import en from './en.json';
 
-export const languages = {
-  es,
-  en,
-};
+export const languages = ['es', 'en'];
 
 export const languageNames = {
   es: es.language_name,
