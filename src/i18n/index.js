073760ac413635ca76dbcb70562c872d295e8aac
jcarlosorte <jcarlosorte@ugr.es>
2025-04-19 22:21:18 +0200

    Update index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,22 +1,8 @@
-// src/i18n/index.js
-import es from './es.json';
-import en from './en.json';
-
-export const languages = {
-  es,
-  en
-};
-
-export const availableLanguages = Object.keys(languages);
-
-export const languageNames = {
-  es: es.language_name,
-  en: en.language_name,
-};
-
-// Inicialización de i18next
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
+import es from './es.json';
+import en from './en.json';
 
 i18n
   .use(initReactI18next)
