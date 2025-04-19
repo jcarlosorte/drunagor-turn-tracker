f541e69726ff3970c3909733cff53aeeb932b2b7
jcarlosorte <jcarlosorte@ugr.es>
2025-04-19 22:21:53 +0200

    Update LanguageContext.jsx

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react';
-import { languages } from '../i18n';
+import { languages } from '../i18n/languageData';
 
 const LanguageContext = createContext();
 
