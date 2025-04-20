e8e0ef9339f6f086a84defbe80a47c1a2f0c26be
jcarlosorte <jcarlosorte@ugr.es>
2025-04-20 15:45:26 +0200

    Create color.js

---
+++ src/data/color.js
@@ -0,0 +1,18 @@
+/ src/data/color.js
+export const ROLES = [
+  {
+    id: 'blanco'
+  },
+  {
+    id: 'gris'
+  },
+  {
+    id: 'negro'
+  },
+  {
+    id: 'comandante'
+  },
+  {
+    id: 'jefe'
+  },
+];
