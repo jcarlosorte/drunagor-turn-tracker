860a16e33a4da8286d6c642da93780061752dbcc
jcarlosorte <jcarlosorte@ugr.es>
2025-04-20 21:17:25 +0200

    Update heroes.js

--- src/data/heroes.js
+++ src/data/heroes.js
@@ -1,57 +1,75 @@
-// src/data/heroes.js
+import lorelai from "@/assets/heroes/Lorelai.png";
+import elros from "@/assets/heroes/Elros.png";
+import jaheen from "@/assets/heroes/Jaheen.png";
+import maya from "@/assets/heroes/Maya.png";
+import vorn from "@/assets/heroes/Vorn.png";
+import drasek from "@/assets/heroes/Drasek.png";
+import jade from "@/assets/heroes/Jade.png";
+import katarina from "@/assets/heroes/Katarina.png";
+import tork from "@/assets/heroes/Tork.png";
+import handuriel from "@/assets/heroes/Handuriel.png";
+import lord_of_wrath from "@/assets/heroes/Lordwrath.png";
+import arkanos from "@/assets/heroes/Arkanos.png";
+import barak from "@/assets/heroes/Barak.png";
+import devron from "@/assets/heroes/Devron.png";
+import duncan from "@/assets/heroes/Duncan.png";
+import flavian from "@/assets/heroes/Flavian.png";
+import kellam from "@/assets/heroes/Kellam.png";
+import pietro from "@/assets/heroes/Pietro.png";
+import savran from "@/assets/heroes/Savran.png";
+import shadow from "@/assets/heroes/Shadow.png";
+import sskar from "@/assets/heroes/Sskar.png";
+import sun from "@/assets/heroes/Sun.png";
+import willow from "@/assets/heroes/Willow.png";
+//import mordred from "@/assets/heroes/Mordred.png";
+//import siff from "@/assets/heroes/Siff.png";
+//import sahara from "@/assets/heroes/Sahara.png";
+//import garamond from "@/assets/heroes/Garamond.png";
+//import diana from "@/assets/heroes/Diana.png";
+//import azriel from "@/assets/heroes/Azriel.png";
+//import brigitte from "@/assets/heroes/Brigitte.png";
+//import drixx from "@/assets/heroes/Drixx.png";
+//import lorien from "@/assets/heroes/Lorien.png";
+//import vacrem from "@/assets/heroes/Vacrem.png";
+//import andreas from "@/assets/heroes/Andreas.png";
+//import nyx from "@/assets/heroes/Nyx.png";
+//import catharina from "@/assets/heroes/Catharina.png";
 
 export const HEROES = [
-  // Juego Base
-  { id: "lorelai", expansionId: "core", baseLife: 10, baseMove: 2, color: "blue", type: "hero" },
-  { id: "elros", expansionId: "core", baseLife: 10, baseMove: 2, color: "red", type: "hero" },
-  { id: "jaheen", expansionId: "core", baseLife: 10, baseMove: 2, color: "green", type: "hero" },
-  { id: "maya", expansionId: "core", baseLife: 10, baseMove: 2, color: "yellow", type: "hero" },
-  { id: "vorn", expansionId: "core", baseLife: 10, baseMove: 2, color: "purple", type: "hero" },
-
-  // El Ascenso del Dragón No Muerto
-  { id: "drasek", expansionId: "undead-dragon", baseLife: 10, baseMove: 2, color: "black", type: "hero" },
-  { id: "jade", expansionId: "undead-dragon", baseLife: 10, baseMove: 2, color: "white", type: "hero" },
-
-  // Desierto Avernal
-  { id: "katarina", expansionId: "infernal-desert", baseLife: 10, baseMove: 2, color: "blue", type: "hero" },
-  { id: "tork", expansionId: "infernal-desert", baseLife: 10, baseMove: 2, color: "red", type: "hero" },
-
-  // Handuriel
-  { id: "handuriel", expansionId: "handuriel", baseLife: 10, baseMove: 2, color: "green", type: "hero" },
-
-  // Señor de la Ira
-  { id: "lord-of-wrath", expansionId: "lord-of-wrath", baseLife: 10, baseMove: 2, color: "yellow", type: "hero" },
-
-  // Botines de Guerra
-  { id: "arkanos", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "blue", type: "hero" },
-  { id: "barak", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "red", type: "hero" },
-  { id: "devron", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "green", type: "hero" },
-  { id: "duncan", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "yellow", type: "hero" },
-  { id: "flavian", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "purple", type: "hero" },
-  { id: "kellam", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "black", type: "hero" },
-  { id: "pietro", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "white", type: "hero" },
-  { id: "savran", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "blue", type: "hero" },
-  { id: "shadow", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "red", type: "hero" },
-  { id: "sskar", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "green", type: "hero" },
-  { id: "sun", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "yellow", type: "hero" },
-  { id: "willow", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, color: "purple", type: "hero" },
-
-  // Apocalypse - Hero Pack 1
-  { id: "mordred", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, color: "black", type: "hero" },
-  { id: "siff", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, color: "white", type: "hero" },
-  { id: "sahara", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, color: "blue", type: "hero" },
-  { id: "garamond", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, color: "red", type: "hero" },
-  { id: "diana", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, color: "green", type: "hero" },
-  { id: "azriel", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, color: "yellow", type: "hero" },
-  { id: "brigitte", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, color: "purple", type: "hero" },
-  { id: "drixx", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, color: "black", type: "hero" },
-
-  // Apocalypse - Lorien
-  { id: "lorien", expansionId: "apocalypse-lorien", baseLife: 10, baseMove: 2, color: "white", type: "hero" },
-
-  // Apocalypse - Awakenings
-  { id: "vacrem", expansionId: "apocalypse-awakenings", baseLife: 10, baseMove: 2, color: "blue", type: "hero" },
-  { id: "andreas", expansionId: "apocalypse-awakenings", baseLife: 10, baseMove: 2, color: "red", type: "hero" },
-  { id: "nyx", expansionId: "apocalypse-awakenings", baseLife: 10, baseMove: 2, color: "green", type: "hero" },
-  { id: "catharina", expansionId: "apocalypse-awakenings", baseLife: 10, baseMove: 2, color: "yellow", type: "hero" },
+  { id: "lorelai", expansionId: "core", baseLife: 10, baseMove: 2, type: "hero", profession: "Maga", image: lorelai },
+  { id: "elros", expansionId: "core", baseLife: 10, baseMove: 2, type: "hero", profession: "Asesino", image: elros },
+  { id: "jaheen", expansionId: "core", baseLife: 10, baseMove: 2, type: "hero", profession: "Clérigo", image: jaheen },
+  { id: "maya", expansionId: "core", baseLife: 10, baseMove: 2, type: "hero", profession: "Arquera", image: maya },
+  { id: "vorn", expansionId: "core", baseLife: 10, baseMove: 2, type: "hero", profession: "Guerrero", image: vorn },
+  { id: "drasek", expansionId: "undead-dragon", baseLife: 10, baseMove: 2, type: "hero", profession: "Paladín", image: drasek },
+  { id: "jade", expansionId: "undead-dragon", baseLife: 10, baseMove: 2, type: "hero", profession: "Paladina", image: jade },
+  { id: "katarina", expansionId: "infernal-desert", baseLife: 10, baseMove: 2, type: "hero", profession: "Bárbara", image: katarina },
+  { id: "tork", expansionId: "infernal-desert", baseLife: 10, baseMove: 2, type: "hero", profession: "Bárbaro", image: tork },
+  { id: "handuriel", expansionId: "handuriel", baseLife: 10, baseMove: 2, type: "hero", profession: "Arquero", image: handuriel },
+  { id: "lord-of-wrath", expansionId: "lord-of-wrath", baseLife: 10, baseMove: 2, type: "hero", profession: "Guerro Sombrío", image: lord_of_wrath },
+  { id: "arkanos", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Mago", image: arkanos },
+  { id: "barak", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Hechicero", image: barak },
+  { id: "devron", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Guerro Sombrío", image: devron },
+  { id: "duncan", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Guerrero", image: duncan },
+  { id: "flavian", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Bardo", image: flavian },
+  { id: "kellam", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Bardo", image: kellam },
+  { id: "pietro", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Clérigo", image: pietro },
+  { id: "savran", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Druida", image: savran },
+  { id: "shadow", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Asesino", image: shadow },
+  { id: "sskar", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Hechicero", image: sskar },
+  { id: "sun", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Monje", image: sun },
+  { id: "willow", expansionId: "spoils-of-war", baseLife: 10, baseMove: 2, type: "hero", profession: "Druida", image: willow },
+  { id: "mordred", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, type: "hero", profession: "paladin", image: mordred },
+  { id: "siff", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, type: "hero", profession: "asesino", image: siff },
+  { id: "sahara", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, type: "hero", profession: "mago", image: sahara },
+  { id: "garamond", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, type: "hero", profession: "bardo", image: garamond },
+  { id: "diana", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, type: "hero", profession: "monje", image: diana },
+  { id: "azriel", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, type: "hero", profession: "clerigo", image: azriel },
+  { id: "brigitte", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, type: "hero", profession: "druida", image: brigitte },
+  { id: "drixx", expansionId: "apocalypse-hero-pack-1", baseLife: 10, baseMove: 2, type: "hero", profession: "asesino", image: drixx },
+  { id: "lorien", expansionId: "apocalypse-lorien", baseLife: 10, baseMove: 2, type: "hero", profession: "mago", image: lorien },
+  { id: "vacrem", expansionId: "apocalypse-awakenings", baseLife: 10, baseMove: 2, type: "hero", profession: "bardo", image: vacrem },
+  { id: "andreas", expansionId: "apocalypse-awakenings", baseLife: 10, baseMove: 2, type: "hero", profession: "barbaro", image: andreas },
+  { id: "nyx", expansionId: "apocalypse-awakenings", baseLife: 10, baseMove: 2, type: "hero", profession: "asesino", image: nyx },
+  { id: "catharina", expansionId: "apocalypse-awakenings", baseLife: 10, baseMove: 2, type: "hero", profession: "monje", image: catharina },
 ];
