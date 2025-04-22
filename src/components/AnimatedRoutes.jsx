c5bdee10894f6ac1e33beef0f1305b70480c17b1
jcarlosorte <jcarlosorte@ugr.es>
2025-04-22 12:05:07 +0200

    Create AnimatedRoutes.jsx

---
+++ src/components/AnimatedRoutes.jsx
@@ -0,0 +1,22 @@
+// src/components/AnimatedRoutes.jsx
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import HomeMenu from "@/App"; // Lo importamos desde App.jsx ya que estaba ahí
+import Tracker from "@/pages/Tracker";
+import Config from "@/pages/Config";
+import InitTracker from "@/pages/InitTracker";
+
+export default function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<HomeMenu />} />
+        <Route path="/tracker" element={<Tracker />} />
+        <Route path="/config" element={<Config />} />
+        <Route path="/init" element={<InitTracker />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
