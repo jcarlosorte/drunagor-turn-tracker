5a38cf279c41c5599a96460ffd825ed6547c4ee9
jcarlosorte <jcarlosorte@ugr.es>
2025-04-22 10:01:51 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import './i18n'; // <- inicializa i18next
 
 import { LanguageProvider } from './context/LanguageContext';
 import { ExpansionProvider } from './context/ExpansionContext';
+import { TrackerProvider } from '@/context/TrackerContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LanguageProvider>
       <ExpansionProvider>
-        <App />
+        <TrackerProvider>
+          <App />
+        </TrackerProvider>
       </ExpansionProvider>
     </LanguageProvider>
   </React.StrictMode>,
