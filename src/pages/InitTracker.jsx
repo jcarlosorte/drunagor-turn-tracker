d9d78c6b37eeeb6a69ef2c9a8be42faea063b829
jcarlosorte <jcarlosorte@ugr.es>
2025-04-26 01:52:17 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -151,7 +151,7 @@ const InitTracker = () => {
     return (
       <div key={index} className={`flex flex-col w-full ${dynamicHeight} py-2`}>
         {/* Sección superior */}
-        <div className="flex items-center justify-center gap-1 flex-wrap overflow-y-auto h-96">
+        <div className="flex items-end justify-center gap-1 flex-wrap overflow-y-auto h-96">
           {heroesAbove?.map(h => (
             <div key={h.id} className="flex flex-col items-center mx-1">
               <div className="mt-1 text-m text-white text-center font-semibold">
@@ -210,7 +210,7 @@ const InitTracker = () => {
         </div>
 
         {/* Sección inferior */}
-        <div className="fflex items-center justify-center gap-1 flex-wrap overflow-y-auto h-96">
+        <div className="flex items-start justify-center gap-1 flex-wrap overflow-y-auto h-96">
           {heroesBelow?.map(h => (
             <div key={h.id} className="flex flex-col items-center mx-1">
               <img
