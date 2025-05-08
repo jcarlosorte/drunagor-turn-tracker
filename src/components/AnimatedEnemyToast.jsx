df3749f1c5c7af72a577897f485fbaf6cd570a63
jcarlosorte <jcarlosorte@ugr.es>
2025-05-08 11:11:29 +0200

    Update AnimatedEnemyToast.jsx

--- src/components/AnimatedEnemyToast.jsx
+++ src/components/AnimatedEnemyToast.jsx
@@ -52,7 +52,7 @@ export default function AnimatedEnemyToast({ enemyData, t }) {
         <div className="text-sm leading-tight space-y-1">
          <div className="font-bold">{name}</div>
          <div>{category} — {com} </div>
-         <div className="text-xs opacity-80">{translatedExpansion}</div>
+      
        </div>
     </motion.div>
   );
