4ac8d6bdfdc0e7b7f36282d66dd23d81e3c35a09
jcarlosorte <jcarlosorte@ugr.es>
2025-05-09 11:14:04 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    {
+      pattern: /drop-shadow-\[0_0_6px_rgba\(.*\)\]/,
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {
