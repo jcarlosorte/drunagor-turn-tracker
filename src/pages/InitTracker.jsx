d202ff86b2a95dbee80c032dbc5d9c1705c681db
jcarlosorte <jcarlosorte@ugr.es>
2025-05-13 16:30:28 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -278,7 +278,7 @@ const InitTracker = () => {
   const EnemyCard = ({ name, comportamiento, categoria, image, position, uuid, color, onRemove }) => (
     <div
       key={uuid}
-      className="flex flex-col items-center mx-1 relative z-10 hover:translate-x-5 hover:translate-y-15transition-transform duration-300"
+      className="flex flex-col items-center mx-1 relative z-10 hover:translate-x-5 hover:translate-y-15 transition-transform duration-300"
     >
       <button
         className="absolute top-1 right-1 text-white bg-red-600 hover:bg-red-700 rounded-full w-6 h-6 flex items-center justify-center z-10"
