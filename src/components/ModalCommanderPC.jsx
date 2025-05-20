a1776af04b324eef62a3d6cb7ec2366aa371e2fa
jcarlosorte <jcarlosorte@ugr.es>
2025-05-20 12:26:59 +0200

    Update ModalCommanderPC.jsx

--- src/components/ModalCommanderPC.jsx
+++ src/components/ModalCommanderPC.jsx
@@ -1,8 +1,12 @@
 // ModalCommanderPC.jsx
 import React, { useState } from 'react';
+import { useLanguage } from '@/context/LanguageContext';
 
-const ModalCommanderPC = ({ onConfirm, onCancel, color = 'gray' }) => {
+const ModalCommanderPC = ({ onConfirm, color = 'gray' }) => {
   const [pcValue, setPcValue] = useState('');
+  const { language, translations } = useLanguage();
+  const ti = translations.trackerInit || {}
+  const ts = translations.trackerSelect || {}
 
   const handleSubmit = () => {
     const parsed = parseInt(pcValue, 10);
@@ -16,7 +20,7 @@ const ModalCommanderPC = ({ onConfirm, onCancel, color = 'gray' }) => {
   return (
     <div className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50`}>
       <div className={`bg-${color}-800 text-white p-4 rounded-xl shadow-lg`}>
-        <h2 className="text-lg font-bold mb-2">Introduce el valor de PC</h2>
+        <h2 className="text-lg font-bold mb-2">{ti.PC}</h2>
         <input
           type="number"
           min="1"
@@ -26,16 +30,10 @@ const ModalCommanderPC = ({ onConfirm, onCancel, color = 'gray' }) => {
         />
         <div className="flex justify-end gap-2 mt-3">
           <button
-            className="bg-gray-300 hover:bg-gray-400 text-black py-1 px-3 rounded"
-            onClick={onCancel}
-          >
-            Cancelar
-          </button>
-          <button
             className={`bg-${color}-600 hover:bg-${color}-700 text-white py-1 px-3 rounded`}
             onClick={handleSubmit}
           >
-            Confirmar
+            {ts.confirm}
           </button>
         </div>
       </div>
