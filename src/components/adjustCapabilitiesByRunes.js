0c3ea9274cd4da48cd30a9ea0bcc87d68b5234c3
jcarlosorte <jcarlosorte@ugr.es>
2025-05-24 01:59:21 +0200

    Update adjustCapabilitiesByRunes.js

--- src/components/adjustCapabilitiesByRunes.js
+++ src/components/adjustCapabilitiesByRunes.js
@@ -1,11 +1,15 @@
 import { runeDependentCapabilities } from '@/components/constants';
 import { useGame } from '@/context/GameContext';
 
-export const adjustCapabilitiesByRunes = (capabilities, runeColor, getRuneCount) => {
-  return capabilities.map(cap => {
+export const adjustCapabilitiesByRunes = (capacidades, runeColor, getRuneCount) => {
+  if (!Array.isArray(capacidades)) {
+    console.warn('⚠️ capacidades no es un array:', capacidades);
+    return [];
+  }
+  return capacidades.map(cap => {
     if (runeDependentCapabilities.includes(cap)) {
       const count = getRuneCount(runeColor);
-      return cap.replace('X', `_${count}`);
+      return cap.replace('X', `${count}`);
     }
     return cap;
   });
