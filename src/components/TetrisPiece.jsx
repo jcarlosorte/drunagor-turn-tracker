3f61f06e45d58bb0ee958325736598eb45b609d1
jcarlosorte <jcarlosorte@ugr.es>
2025-05-26 21:51:02 +0200

    Create TetrisPiece.jsx

---
+++ src/components/TetrisPiece.jsx
@@ -0,0 +1,67 @@
+// src/components/TetrisPiece.jsx
+import React from 'react';
+import { GiBrickWall } from 'react-icons/gi';
+
+const blockSize = 24; // tamaño del icono (en px)
+
+const TETRIS_SHAPES = {
+  I: [
+    [1, 1, 1, 1],
+  ],
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+  T: [
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  L: [
+    [1, 0],
+    [1, 0],
+    [1, 1],
+  ],
+  J: [
+    [0, 1],
+    [0, 1],
+    [1, 1],
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+  ]
+};
+
+const TetrisPiece = ({ type = 'T', color = 'text-blue-500' }) => {
+  const shape = TETRIS_SHAPES[type];
+
+  return (
+    <div className="inline-block">
+      {shape.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex h-[24px]">
+          {row.map((cell, colIndex) =>
+            cell ? (
+              <div
+                key={colIndex}
+                className={`w-[${blockSize}px] h-[${blockSize}px] flex items-center justify-center ${color}`}
+              >
+                <GiBrickWall size={blockSize} />
+              </div>
+            ) : (
+              <div
+                key={colIndex}
+                className={`w-[${blockSize}px] h-[${blockSize}px]`}
+              />
+            )
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TetrisPiece;
