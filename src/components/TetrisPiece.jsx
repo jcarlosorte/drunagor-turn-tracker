3e4cf8fd3f238dd6916ef2066515adf1121d6095
jcarlosorte <jcarlosorte@ugr.es>
2025-05-27 10:42:05 +0200

    Update TetrisPiece.jsx

--- src/components/TetrisPiece.jsx
+++ src/components/TetrisPiece.jsx
@@ -2,68 +2,60 @@
 import React from 'react';
 import { GiBrickWall } from 'react-icons/gi';
 
-const blockSize = 24; // tamaño del icono (en px)
+const blockSize = 24;
 
 const TETRIS_SHAPES = {
-  II: [
-    [1, 1, 1, 1],
-  ],
-  I: [
-    [1, 1],
-  ],
-  T: [
-    [0, 1, 0],
-    [1, 1, 1],
-  ],
-  L: [
-    [1, 0],
-    [1, 1],
-  ],
-  J: [
-    [0, 1],
-    [0, 1],
-    [1, 1],
-  ],
-  S: [
-    [1, 1, 0],
-    [0, 1, 1],
-  ],
-  Z: [
-    [1, 1, 0],
-    [0, 1, 0],
-    [0, 1, 1],
-  ],
-  U: [
-    [1, 0, 1],
-    [1, 1, 1],
-  ],
-  Y: [
-    [1, 0, 1],
-    [1, 1, 1],
-    [0, 1, 0],
-  ]
+  II: [[1, 1, 1, 1]],
+  I: [[1, 1]],
+  T: [[0, 1, 0], [1, 1, 1]],
+  L: [[1, 0], [1, 1]],
+  J: [[0, 1], [0, 1], [1, 1]],
+  S: [[1, 1, 0], [0, 1, 1]],
+  Z: [[1, 1, 0], [0, 1, 0], [0, 1, 1]],
+  U: [[1, 0, 1], [1, 1, 1]],
+  Y: [[1, 0, 1], [1, 1, 1], [0, 1, 0]]
 };
 
 const TetrisPiece = ({ type = 'T', color = 'bg-blue-500' }) => {
   const shape = TETRIS_SHAPES[type];
 
   return (
-    <div className="inline-block">
+    <div className="inline-block leading-none">
       {shape.map((row, rowIndex) => (
         <div key={rowIndex} className="flex" style={{ height: `${blockSize}px` }}>
           {row.map((cell, colIndex) =>
             cell ? (
               <div
                 key={colIndex}
-                className={`flex items-center justify-center ${color} rounded-full`}
-                style={{ width: `${blockSize}px`, height: `${blockSize}px`, lineHeight: 0, padding: 0, margin: 0 }}
+                className={`flex items-center justify-center ${color}`}
+                style={{
+                  width: `${blockSize}px`,
+                  height: `${blockSize}px`,
+                  lineHeight: 0,
+                  padding: 0,
+                  margin: 0
+                }}
               >
-                <GiBrickWall size={blockSize - 4} className="text-white" />
+                <GiBrickWall
+                  size={blockSize}
+                  className="text-white"
+                  style={{
+                    display: 'block',
+                    lineHeight: 0,
+                    margin: 0,
+                    padding: 0
+                  }}
+                />
               </div>
             ) : (
               <div
                 key={colIndex}
-                style={{ width: `${blockSize}px`, height: `${blockSize}px`, padding: 0, margin: 0  }}
+                style={{
+                  width: `${blockSize}px`,
+                  height: `${blockSize}px`,
+                  padding: 0,
+                  margin: 0
+                }}
               />
             )
           )}
