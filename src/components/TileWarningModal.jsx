4786e5bf22ad28cd0b0a2904e2a23ca6f70918af
jcarlosorte <jcarlosorte@ugr.es>
2025-05-27 11:27:19 +0200

    Update TileWarningModal.jsx

--- src/components/TileWarningModal.jsx
+++ src/components/TileWarningModal.jsx
@@ -10,7 +10,7 @@ const TileWarningModal = () => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg text-center max-w-sm w-full">
-        <h2 className="text-lg font-bold mb-2">⚠️ Aviso </h2>
+        <h2 className="text-lg font-bold text-gray-800 mb-2">⚠️ HAS PERDIDO ⚠️</h2>
         <p className="text-sm text-gray-800 mb-4">{tileWarning}</p>
         <button
           className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
