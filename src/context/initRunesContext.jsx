222833eba24d2e9f7235c5cc8c263b1fe66a2f45
jcarlosorte <jcarlosorte@ugr.es>
2025-05-30 01:05:57 +0200

    Update initRunesContext.jsx

--- src/context/initRunesContext.jsx
+++ src/context/initRunesContext.jsx
@@ -1,34 +1,43 @@
-import React, { createContext, useContext, useState } from 'react'; import { v4 as uuidv4 } from 'uuid';
+import React, { createContext, useContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useLanguage } from "@/context/LanguageContext";
 
 const InitRunesContext = createContext();
 
-export const InitRunesProvider = ({ children }) => { const [placedRunes, setPlacedRunes] = useState([]);
+export const InitRunesProvider = ({ children }) => { 
+  const { translations } = useLanguage();
+  const t = translations?.trackerInit || {};
+  const [placedRunes, setPlacedRunes] = useState([]);
 
-const placeRune = ({ rune }) => {
-  // Verificar si ya existe una carta con el mismo id y cara
-  const exists = placedRunes.some(
-    (r) => r.rune.id === rune.id
-  );
-  if (exists) return; // Evitar duplicados
-
-  setPlacedRunes(prev => [
-    ...prev,
-    {
-      rune: {
-        uuid: uuidv4(),
-        ...rune,
-        colorIndex: 10, // siempre se colocan en índice 10
+  const placeRune = ({ rune }) => {
+    // Verificar si ya existe una carta con el mismo id y cara
+    const exists = placedRunes.some(
+      (r) => r.rune.id === rune.id
+    );
+    if (exists) {
+      alert(t.RuneRepeat);
+      return;
+    }
+    setPlacedRunes(prev => [
+      ...prev,
+      {
+        rune: {
+          uuid: uuidv4(),
+          ...rune,
+          colorIndex: 10, // siempre se colocan en índice 10
+        },
       },
-    },
-  ]);
+    ]);
+  };
+  
+  
+  const removeRuneByUUID = (uuid) => { setPlacedRunes(prev => prev.filter(r => r.rune.uuid !== uuid)); };
+  
+  const resetPlacedRunes = () => { setPlacedRunes([]); };
+  
+  return ( <InitRunesContext.Provider value={{ placedRunes, placeRune, removeRuneByUUID, resetPlacedRunes, setPlacedRunes }}> {children} </InitRunesContext.Provider> ); 
+
 };
-
-
-const removeRuneByUUID = (uuid) => { setPlacedRunes(prev => prev.filter(r => r.rune.uuid !== uuid)); };
-
-const resetPlacedRunes = () => { setPlacedRunes([]); };
-
-return ( <InitRunesContext.Provider value={{ placedRunes, placeRune, removeRuneByUUID, resetPlacedRunes, setPlacedRunes }}> {children} </InitRunesContext.Provider> ); };
 
 export const useInitRunes = () => useContext(InitRunesContext);
 
