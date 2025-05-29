932600d59e1e9bf88e062779714cfb9be21da6c3
jcarlosorte <jcarlosorte@ugr.es>
2025-05-30 01:59:55 +0200

    Update initRunesContext.jsx

--- src/context/initRunesContext.jsx
+++ src/context/initRunesContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import { useLanguage } from "@/context/LanguageContext";
+import { v4 as uuidv4 } from 'uuid';
 
 const InitRunesContext = createContext();
 
