fc257188f25bf11ee12f71c7b77d07a65922fc76
jcarlosorte <jcarlosorte@ugr.es>
2025-05-30 00:57:44 +0200

    Update initRunesContext.jsx

--- src/context/initRunesContext.jsx
+++ src/context/initRunesContext.jsx
@@ -4,7 +4,25 @@ const InitRunesContext = createContext();
 
 export const InitRunesProvider = ({ children }) => { const [placedRunes, setPlacedRunes] = useState([]);
 
-const placeRune = ({ rune }) => { setPlacedRunes(prev => [ ...prev, { rune: { uuid: uuidv4(), ...rune, }, } ]); };
+const placeRune = ({ rune }) => {
+  // Verificar si ya existe una carta con el mismo id y cara
+  const exists = placedRunes.some(
+    (r) => r.rune.id === rune.id
+  );
+  if (exists) return; // Evitar duplicados
+
+  setPlacedRunes(prev => [
+    ...prev,
+    {
+      rune: {
+        uuid: uuidv4(),
+        ...rune,
+        colorIndex: 10, // siempre se colocan en índice 10
+      },
+    },
+  ]);
+};
+
 
 const removeRuneByUUID = (uuid) => { setPlacedRunes(prev => prev.filter(r => r.rune.uuid !== uuid)); };
 
