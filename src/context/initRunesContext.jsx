8c0b25fd055562623213262e1d6478822ede8e78
jcarlosorte <jcarlosorte@ugr.es>
2025-05-30 02:04:46 +0200

    Update initRunesContext.jsx

--- src/context/initRunesContext.jsx
+++ src/context/initRunesContext.jsx
@@ -1,12 +1,11 @@
 import React, { createContext, useContext, useState } from 'react';
-import { useLanguage } from "@/context/LanguageContext";
+
 import { v4 as uuidv4 } from 'uuid';
 
 const InitRunesContext = createContext();
 
 export const InitRunesProvider = ({ children }) => { 
-  const { translations } = useLanguage();
-  const t = translations?.trackerInit || {};
+
   const [placedRunes, setPlacedRunes] = useState([]);
 
   const placeRune = ({ rune }) => {
@@ -15,7 +14,7 @@ export const InitRunesProvider = ({ children }) => {
       (r) => r.rune.id === rune.id
     );
     if (exists) {
-      alert(t.RuneRepeat);
+      alert("");
       return;
     }
     setPlacedRunes(prev => [
