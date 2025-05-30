e881bef5b46cc18d84ef55b797ec7b7d01806283
jcarlosorte <jcarlosorte@ugr.es>
2025-05-30 13:47:15 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import './i18n'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './i18n';
 
 import { LanguageProvider } from './context/LanguageContext';
 import { ExpansionProvider } from './context/ExpansionContext';
-import { TrackerProvider } from '@/context/TrackerContext';
+import { TrackerProvider } from './context/TrackerContext';
 import { GameProvider } from './context/GameContext';
-import { InitRunesProvider } from '@/context/InitRunesContext';
-import { InitEnemiesProvider } from '@/context/InitEnemiesContext'; // MOVIDO AQUÍ
+import { InitRunesProvider } from './context/InitRunesContext';
+import { InitEnemiesProvider } from './context/InitEnemiesContext';
+
+import AppWrapper from './AppWrapper'; // 👈 nuevo
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,8 +19,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <TrackerProvider> 
           <GameProvider>
             <InitRunesProvider>
-              <InitEnemiesProvider> {/* ✅ MOVIDO AQUÍ */}
-                <App />
+              <InitEnemiesProvider>
+                <AppWrapper /> {/* 👈 nuevo componente que controla el inicio */}
               </InitEnemiesProvider>
             </InitRunesProvider>
           </GameProvider>
@@ -28,4 +28,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </ExpansionProvider>
     </LanguageProvider>
   </React.StrictMode>,
-)
+);
