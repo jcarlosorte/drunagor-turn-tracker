e6ee9022284605deab18a40aca44abd57195f78b
jcarlosorte <jcarlosorte@ugr.es>
2025-05-30 02:17:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import './i18n'; // <- inicializa i18next
+import './i18n'
 
 import { LanguageProvider } from './context/LanguageContext';
 import { ExpansionProvider } from './context/ExpansionContext';
 import { TrackerProvider } from '@/context/TrackerContext';
 import { GameProvider } from './context/GameContext';
 import { InitRunesProvider } from '@/context/InitRunesContext';
-
+import { InitEnemiesProvider } from '@/context/InitEnemiesContext'; // MOVIDO AQUÍ
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,7 +19,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <TrackerProvider> 
           <GameProvider>
             <InitRunesProvider>
-              <App />
+              <InitEnemiesProvider> {/* ✅ MOVIDO AQUÍ */}
+                <App />
+              </InitEnemiesProvider>
             </InitRunesProvider>
           </GameProvider>
         </TrackerProvider>
@@ -27,3 +29,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </LanguageProvider>
   </React.StrictMode>,
 )
+
