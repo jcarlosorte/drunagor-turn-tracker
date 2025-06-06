3cec42c89ba9356ec957546baca60824de928d28
jcarlosorte <jcarlosorte@ugr.es>
2025-06-06 12:44:45 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -25,7 +25,7 @@ import { ModalEnemyCard } from '@/components/ModalEnemyCard';
 import { adjustCapabilitiesByRunes } from '@/components/adjustCapabilitiesByRunes';
 import { useRenderEnemyCapabilities } from '@/components/renderEnemyCapabilities';
 import TileWarningModal from '@/components/TileWarningModal';
-import TileToast from '@/data/TileToast';
+import TileToast from '@/components/TileToast';
 import CommanderCard from '@/components/CommanderCard';
 import { v4 as uuidv4 } from 'uuid';
 
