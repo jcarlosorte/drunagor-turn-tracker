1600efa711874294cd4231ae4d0984ddc02c2b71
jcarlosorte <jcarlosorte@ugr.es>
2025-06-09 11:28:36 +0200

    Update CommanderCard.jsx

--- src/components/CommanderCard.jsx
+++ src/components/CommanderCard.jsx
@@ -14,7 +14,7 @@ const CommanderCard = ({ carta }) => {
     <>
       <div
         onClick={() => setShowModal(true)}
-        className="cursor-pointer relative w-full bg-black p-2 rounded-lg border-2 border-yellow-400 shadow-md hover:scale-105 transition"
+        className="cursor-pointer relative w-full max-w-[140px] items-center bg-black p-2 rounded-lg border-2 border-yellow-400 shadow-md hover:scale-105 transition"
       >
         <div
           className="text-sm text-yellow-300 text-center"
