bea1f40e6cb8622a40df7cf78c0396745fb8498f
jcarlosorte <jcarlosorte@ugr.es>
2025-06-10 12:15:22 +0200

    Create turnOrder.js

---
+++ src/data/turnOrder.js
@@ -0,0 +1,6 @@
+// src/data/turnOrder.js
+export const TURN_ORDER = [
+  { type: 'hero', role: 'defensor', position: 'arriba', index: 0 },
+  { type: 'enemy', rune: 'naranja', position: 'arriba', index: 1 },
+  ...
+];
