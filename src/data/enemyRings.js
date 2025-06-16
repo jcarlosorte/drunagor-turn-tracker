77cfff64e4757275eb81afc6b12eccadf78f8191
jcarlosorte <jcarlosorte@ugr.es>
2025-06-16 12:35:37 +0200

    Create enemyRings.js

---
+++ src/data/enemyRings.js
@@ -0,0 +1,8 @@
+export const ENEMY_RING_COLORS = [
+  { id: 'amarillo', name: 'Amarillo', className: 'ring-yellow-400' },
+  { id: 'azul',     name: 'Azul',     className: 'ring-blue-400' },
+  { id: 'rosa',     name: 'Rosa',     className: 'ring-pink-400' },
+  { id: 'verde',    name: 'Verde',    className: 'ring-green-400' },
+  { id: 'rojo',     name: 'Rojo',     className: 'ring-red-400' },
+  { id: 'gris',     name: 'Gris',     className: 'ring-gray-400' },
+];
