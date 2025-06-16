4e5d9f4a6a42d19dd5cbd0cd77d26c9a35bdb201
jcarlosorte <jcarlosorte@ugr.es>
2025-06-16 15:34:01 +0200

    Create enemyRingsBig.js

---
+++ src/data/enemyRingsBig.js
@@ -0,0 +1,6 @@
+export const ENEMY_RING_COLORS = [
+  { id: 'amarillo', name: 'Amarillo', className: 'ring-4 ring-yellow-400' },
+  { id: 'azul_marino', name: 'Azul marino', className: 'ring-4 ring-indigo-700' },
+  { id: 'rosa',     name: 'Rosa',     className: 'ring-4 ring-pink-400' },
+  { id: 'verde',    name: 'Verde',    className: 'ring-4 ring-green-400' },
+];
