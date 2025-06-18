25bf0de7cad0b76d579d9586ad3fd4572f4542d7
jcarlosorte <jcarlosorte@ugr.es>
2025-06-18 09:42:21 +0200

    Update cartasEspeciales.js

--- src/data/cartasEspeciales.js
+++ src/data/cartasEspeciales.js
@@ -71,6 +71,79 @@ export const CARTAS_COMANDANTE = [
   }
 ];
 
+export const CARTAS_OVERLORD = [
+  {
+    id: 'over_1',
+    nombre: 'over_1',
+    rune: 'azul',
+    runePosition: 'arriba',
+    capacidades: 'over_1'
+  },
+  {
+    id: 'over_2',
+    nombre: 'over_2',
+    rune: 'azul',
+    runePosition: 'abajo',
+    capacidades: 'over_2'
+  },
+  {
+    id: 'over_3',
+    nombre: 'over_3',
+    rune: 'gris',
+    runePosition: 'arriba',
+    capacidades: 'over_3'
+  },
+   {
+    id: 'over_4',
+    nombre: 'over_4',
+    rune: 'gris',
+    runePosition: 'abajo',
+    capacidades: 'over_4'
+  },
+   {
+    id: 'over_5',
+    nombre: 'over_5',
+    rune: 'rojo',
+    runePosition: 'arriba',
+    capacidades: 'over_5'
+  },
+   {
+    id: 'over_6',
+    nombre: 'over_6',
+    rune: 'rojo',
+    runePosition: 'abajo',
+    capacidades: 'over_6'
+  },
+  {
+    id: 'over_7',
+    nombre: 'over_7',
+    rune: 'verde',
+    runePosition: 'arriba',
+    capacidades: 'over_7'
+  },
+   {
+    id: 'over_8',
+    nombre: 'over_8',
+    rune: 'verde',
+    runePosition: 'abajo',
+    capacidades: 'over_8'
+  },
+   {
+    id: 'over_9',
+    nombre: 'over_9',
+    rune: 'naranja',
+    runePosition: 'arriba',
+    capacidades: 'over_9'
+  },
+   {
+    id: 'over_10',
+    nombre: 'over_10',
+    rune: 'naranja',
+    runePosition: 'abajo',
+    capacidades: 'over_10'
+  }
+];
+
 export const CARTAS_JEFE = [
   {
     id: 'boss_1',
