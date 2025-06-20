7f01449a1100a0194f0b7e92a40c8eadf39733bd
jcarlosorte <jcarlosorte@ugr.es>
2025-06-20 22:22:12 +0200

    Update ModalEnemyCard.jsx

--- src/components/ModalEnemyCard.jsx
+++ src/components/ModalEnemyCard.jsx
@@ -44,7 +44,7 @@ export const ModalEnemyCard = ({ uuid, enemy, onClose, onDelete, onVidaChange })
     gris: 'border-gris',
     negro: 'border-negro',
     comandante: 'border-dorado',
-    hero: 'border-morado',
+    hero: 'border-dorado',
     jefe: 'border-morado',
   };
 
@@ -53,8 +53,8 @@ export const ModalEnemyCard = ({ uuid, enemy, onClose, onDelete, onVidaChange })
     gris: 'bg-gray-500',
     negro: 'bg-black',
     comandante: 'bg-yellow-400',
-    hero: 'bg-purple-700',
-    jefe: 'bg-purple-700',
+    hero: 'bg-orange-400',
+    jefe: 'bg-purple-400',
   };
   
   const textBgColorMap = {
@@ -62,7 +62,7 @@ export const ModalEnemyCard = ({ uuid, enemy, onClose, onDelete, onVidaChange })
     gris: 'bg-gray-500/70',
     negro: 'bg-black/70',
     comandante: 'bg-yellow-400/70',
-    hero: 'bg-purple-700/70',
+    hero: 'bg-orange-400/70',
     jefe: 'bg-purple-700/70',
   };
 
