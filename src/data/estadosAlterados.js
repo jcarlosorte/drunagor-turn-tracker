e81b54cbd5878a64501ff8a2465cd86802172f06
jcarlosorte <jcarlosorte@ugr.es>
2025-07-09 10:39:25 +0200

    Update and rename cartasEspeciales_o.js to estadosAlterados.js

---
+++ src/data/estadosAlterados.js
@@ -0,0 +1,12 @@
+export const ESTADOS_ALTERADOS = [
+  {id: "HEMORRAGIA_X", reduce:""},
+  {id: "DERRIBO_X", reduce:""},
+  {id: "SIGILO_X", reduce:""},
+  {id: "ATURDIMIENTO_X", reduce:""},
+  {id: "VENENO_X", reduce:""},
+  {id: "INTIMIDAR_X", reduce:""},
+  {id: "LENTITUD_X", reduce:""},
+  {id: "QUEMADURA_X", reduce:""},
+  {id: "MARCA_DE_MONTARAZ", reduce:""},
+  {id: "ESCUDO_X", reduce:""},
+];
