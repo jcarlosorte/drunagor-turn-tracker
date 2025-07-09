e27ca163ebf4f21dcde94980c290c105a15a75f3
jcarlosorte <jcarlosorte@ugr.es>
2025-07-09 11:38:14 +0200

    Create incrusion.js

---
+++ src/data/incrusion.js
@@ -0,0 +1,4 @@
+export const INCRUSION = [
+  { id: "incru1", cara: "A", posicion: "abajo", nombre: "Incru", accion: "Roba", numRunas: 1  },
+  { id: "incru1", cara: "B", posicion: "abajo", nombre: "Incru", accion: "Roba", numRunas: 2  }
+];
