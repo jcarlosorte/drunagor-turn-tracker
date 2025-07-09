d8d3ac3cccfb614b1ddbcfce17ade305fff1ee76
jcarlosorte <jcarlosorte@ugr.es>
2025-07-09 12:30:33 +0200

    Create defensa.js

---
+++ src/data/defensa.js
@@ -0,0 +1,4 @@
+export const INCRUSION = [
+  { id: "defen1", cara: "A", posicion: "arriba", nombre: "Defen_1", accion: "Roba", num: 1, tipo: "aldeano"  },
+  { id: "defen1", cara: "B", posicion: "arriba", nombre: "Defen_2", accion: "Roba", num: 1, tipo: "errantes"  }
+];
