99cb4d64d807e385ca2b8ee607cb4fa5fd961984
jcarlosorte <jcarlosorte@ugr.es>
2025-07-14 21:41:48 +0200

    Update defensa.js

--- src/data/defensa.js
+++ src/data/defensa.js
@@ -1,4 +1,4 @@
 export const DEFENSA = [
-  { id: "defen1", cara: "A", posicion: "arriba", nombre: "Defen_1", accion: "Roba", num: 1, tipo: "defensa", carta: "aldeano"  },
-  { id: "defen1", cara: "B", posicion: "arriba", nombre: "Defen_2", accion: "Roba", num: 1, tipo: "defensa", carta: "errantes"  }
+  { id: "defen1", cara: "A", posicion: "arriba", nombre: "Defen_1", accion: "Roba", numRunas: 1, tipo: "defensa", carta: "aldeano"  },
+  { id: "defen1", cara: "B", posicion: "arriba", nombre: "Defen_2", accion: "Roba", numRunas: 1, tipo: "defensa", carta: "errantes"  }
 ];
