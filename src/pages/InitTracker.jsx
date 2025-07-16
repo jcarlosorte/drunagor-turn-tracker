461d56dd87082366aca65ab847e9464e31299985
jcarlosorte <jcarlosorte@ugr.es>
2025-07-16 10:28:55 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -992,7 +992,7 @@ const InitTracker = () => {
     };
   
     return (
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[140px] perspective z-50 hover:scale-105">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[140px] perspective hover:scale-105">
         <div
           className={`relative w-full h-full transition-transform duration-700 transform-style preserve-3d ${
             flipped ? 'rotate-y-180' : ''
