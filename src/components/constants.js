b82c3ff28c5bf5024c91da11b9a89d4ea048d012
jcarlosorte <jcarlosorte@ugr.es>
2025-08-17 22:53:10 +0200

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
 // constants.js
-export const runeDependentCapabilities = ['ESCUDO X', 'SANAR X', 'FATIGA X', 'INTIMIDAR X', 'EMPUJON X', 'REGENERACION X', 'QUEMADURA X'];
+export const runeDependentCapabilities = ['ESCUDO X', 'SANAR X', 'FATIGA X', 'INTIMIDAR X', 'EMPUJON X', 'REGENERACION X', 'QUEMADURA X', 'ABSORBER SALUD X'];
