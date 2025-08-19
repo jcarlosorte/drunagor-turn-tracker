3ef23ffea4078573501b676b33c0049148fdcd95
jcarlosorte <jcarlosorte@ugr.es>
2025-08-19 17:08:03 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -1201,12 +1201,14 @@ const InitTracker = () => {
               <label>{ti.applyEffect}</label>
               
             </div>
+            <div className="flex items-center justify-center">
             {/* Badge de contador si es defensa */}
               {tipo === "defensa" && defenseCounter > 0 && (
-                <div className="bottom-1 text-center bg-red-600 text-white text-xs font-bold px-2 py-1 rounded-full shadow">
+                <div className="text-center bg-red-600 text-white text-xs font-bold px-2 py-1 rounded-full shadow">
                   {ti.contador} {defenseCounter}
                 </div>
               )}
+            </div>
           </div>
         </div>
       );
