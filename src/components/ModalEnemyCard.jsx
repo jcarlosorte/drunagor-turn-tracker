df333b804a371471f89c5f6a3137cdfbbd010342
jcarlosorte <jcarlosorte@ugr.es>
2025-08-31 01:21:47 +0200

    Update ModalEnemyCard.jsx

--- src/components/ModalEnemyCard.jsx
+++ src/components/ModalEnemyCard.jsx
@@ -29,6 +29,17 @@ export const ModalEnemyCard = ({ uuid, enemy, onClose, onDelete, onVidaChange, o
     }
   }, [enemy]);
 
+    if (!enemy) {
+    //console.log(enemy);
+    //return (
+    return;
+      //<div className="modal">
+        //<p>{ti.enemyNotFound}</p>
+        //<button onClick={onClose}>{ti.close}</button>
+      //</div>
+    //);
+  }
+
   const { id, name, rune, imagen, vida, vidaMax, movimiento, ataque, color, comportamiento, categoria, inmunidad, tipo_ataque, capacidades, capacidadesOriginales } = enemy;
   const numeroIconos = [    <MdLooksOne key="1" />,    <MdLooksTwo key="2" />,    <MdLooks3 key="3" />,    <MdLooks4 key="4" />,    <MdLooks5 key="5" />,    <MdLooks6 key="6" />,  ];
   const capacidadesAjustadas = adjustCapabilitiesByRunes(capacidadesOriginales, rune, getRuneCount);
