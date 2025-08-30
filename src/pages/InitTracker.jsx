d5b6f806cfa6836dfc608ebef307c78ae770e098
jcarlosorte <jcarlosorte@ugr.es>
2025-08-30 02:52:20 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -1812,7 +1812,6 @@ const InitTracker = () => {
                 animate={{ opacity: 1, scale: 1, y: 0 }}
                 exit={{ opacity: 0, scale: 0.95, y: 8 }}
                 transition={{ duration: 0.22 }}
-                className={`animate-pulse`}
               >
                 <TileToast
                   tile={tile}
