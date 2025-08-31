56cf66d7bf5a5cd57af5f0cdb110995e0d4a6871
jcarlosorte <jcarlosorte@ugr.es>
2025-09-01 01:15:32 +0200

    Update estadosAlterados.js

--- src/data/estadosAlterados.js
+++ src/data/estadosAlterados.js
@@ -1,14 +1,32 @@
+import derribo from "@/assets/assets_logo/derribo.png";
+import marca from "@/assets/assets_logo/montaraz.png";
+import sigilo from "@/assets/assets_logo/sigilo.png";
+import aturdimiento from "@/assets/assets_logo/aturdimiento.png";
+import lentitud from "@/assets/assets_logo/lentitud.png";
 import hemorragia from "@/assets/assets_logo/hemorragia_1.png";
+import veneno from "@/assets/assets_logo/veneno_1.png";
+import intimidar from "@/assets/assets_logo/intimidar_1.png";
+import quemadura from "@/assets/assets_logo/quemadura_1.png";
+import escudo from "@/assets/assets_logo/escudo_1.png";
 
 export const ESTADOS_ALTERADOS = [
-  {id: "HEMORRAGIA_X", reduce:""},
-  {id: "DERRIBO_X", reduce:""},
-  {id: "SIGILO_X", reduce:""},
-  {id: "ATURDIMIENTO_X", reduce:""},
-  {id: "VENENO_X", reduce:""},
-  {id: "INTIMIDAR_X", reduce:""},
-  {id: "LENTITUD_X", reduce:""},
-  {id: "QUEMADURA_X", reduce:""},
-  {id: "MARCA_DE_MONTARAZ", reduce:""},
-  {id: "ESCUDO_X", reduce:""}
+  // condiciones redundantes
+  { id: "DERRIBO_X", texto: "derribo", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: derribo },
+  { id: "MARCA_DE_MONTARAZ", texto: "marca de montaraz", reduce: "no", numReduce: 0, turno: "", daño: 0, max: 1, imagen: marca },
+  { id: "SIGILO_X", texto: "sigilo", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: sigilo },
+  { id: "ATURDIMIENTO_X", texto: "aturdimiento", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: aturdimiento },
+  { id: "LENTITUD_X", texto: "lentitud", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: lentitud },
+
+  // condiciones acumulables
+  { id: "HEMORRAGIA_X", texto: "hemorragia", reduce: "si", numReduce: 4, turno: "principio", daño: 1, max: 4, imagen: hemorragia },
+  { id: "VENENO_X", texto: "veneno", reduce: "no", numReduce: 0, turno: "principio", daño: 1, max: 4, imagen: veneno },
+  { id: "INTIMIDAR_X", texto: "intimidar", reduce: "si", numReduce: 4, turno: "final", daño: 0, max: 4, imagen: intimidar },
+  { id: "QUEMADURA_X", texto: "quemadura", reduce: "si", numReduce: 1, turno: "principio", daño: 1, max: 4, imagen: quemadura },
+
+  // recurso acumulable
+  //{id: "FURIA", reduce:"", max: 4},
+  //{id: "FRUTO_DE_VIDA", reduce:"", max: 4},
+  //{id: "CONCENTRACION_X", reduce:"", max: 4},
+  //{id: "KI", reduce:"", max: 4},
+  { id: "ESCUDO_X", texto: "escudo", reduce: "no", numReduce: 0, turno: "", daño: 0, max: 4, imagen: escudo }
 ];
