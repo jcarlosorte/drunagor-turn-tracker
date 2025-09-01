90c6cb4efd0632fa5f0e028097c74932f1c3505f
jcarlosorte <jcarlosorte@ugr.es>
2025-09-01 21:49:43 +0200

    Update estadosAlterados.js

--- src/data/estadosAlterados.js
+++ src/data/estadosAlterados.js
@@ -11,22 +11,22 @@ import escudo from "@/assets/assets_logo/escudo.png";
 
 export const ESTADOS_ALTERADOS = [
   // condiciones redundantes
-  { id: "DERRIBO_X", texto: "derribo", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: derribo },
+  { id: "DERRIBO_I", texto: "derribo", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: derribo },
   { id: "MARCA_DE_MONTARAZ", texto: "marca de montaraz", reduce: "no", numReduce: 0, turno: "", daño: 0, max: 1, imagen: marca },
-  { id: "SIGILO_X", texto: "sigilo", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: sigilo },
-  { id: "ATURDIMIENTO_X", texto: "aturdimiento", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: aturdimiento },
-  { id: "LENTITUD_X", texto: "lentitud", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: lentitud },
+  { id: "SIGILO_I", texto: "sigilo", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: sigilo },
+  { id: "ATURDIMIENTO_I", texto: "aturdimiento", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: aturdimiento },
+  { id: "LENTITUD_I", texto: "lentitud", reduce: "si", numReduce: 1, turno: "principio", daño: 0, max: 1, imagen: lentitud },
 
   // condiciones acumulables
-  { id: "HEMORRAGIA_X", texto: "hemorragia", reduce: "si", numReduce: 4, turno: "principio", daño: 1, max: 4, imagen: hemorragia },
-  { id: "VENENO_X", texto: "veneno", reduce: "no", numReduce: 0, turno: "principio", daño: 1, max: 4, imagen: veneno },
-  { id: "INTIMIDAR_X", texto: "intimidar", reduce: "si", numReduce: 4, turno: "final", daño: 0, max: 4, imagen: intimidar },
-  { id: "QUEMADURA_X", texto: "quemadura", reduce: "si", numReduce: 1, turno: "principio", daño: 1, max: 4, imagen: quemadura },
+  { id: "HEMORRAGIA_I", texto: "hemorragia", reduce: "si", numReduce: 4, turno: "principio", daño: 1, max: 4, imagen: hemorragia },
+  { id: "VENENO_I", texto: "veneno", reduce: "no", numReduce: 0, turno: "principio", daño: 1, max: 4, imagen: veneno },
+  { id: "INTIMIDAR_I", texto: "intimidar", reduce: "si", numReduce: 4, turno: "final", daño: 0, max: 4, imagen: intimidar },
+  { id: "QUEMADURA_I", texto: "quemadura", reduce: "si", numReduce: 1, turno: "principio", daño: 1, max: 4, imagen: quemadura },
 
   // recurso acumulable
   //{id: "FURIA", reduce:"", max: 4},
   //{id: "FRUTO_DE_VIDA", reduce:"", max: 4},
   //{id: "CONCENTRACION_X", reduce:"", max: 4},
   //{id: "KI", reduce:"", max: 4},
-  { id: "ESCUDO_X", texto: "escudo", reduce: "no", numReduce: 0, turno: "", daño: 0, max: 4, imagen: escudo }
+  { id: "ESCUDO", texto: "escudo", reduce: "no", numReduce: 0, turno: "", daño: 0, max: 4, imagen: escudo }
 ];
