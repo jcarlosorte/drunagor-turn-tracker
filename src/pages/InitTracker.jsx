7e0e7f5b45a4343d6719db3995b476ca7538a30f
jcarlosorte <jcarlosorte@ugr.es>
2025-09-04 00:18:49 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -1566,7 +1566,7 @@ const InitTracker = () => {
     };
   
     return (
-      <div className={`${rune.posicion === 'abajo' ? 'absolute bottom-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' : 'absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[140px] perspective hover:scale-105`}>
+      <div className={`${rune.posicion === 'abajo' ? 'absolute bottom-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' : 'absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2'} w-[140px] perspective hover:scale-105`}>
         
         <div
           className={`relative w-full h-full transition-transform duration-700 transform-style preserve-3d ${
