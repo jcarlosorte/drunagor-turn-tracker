4cd7fa155dca24f4cc85f77d0e43b7c4fe1a4c3f
jcarlosorte <jcarlosorte@ugr.es>
2025-09-04 01:53:12 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -1459,8 +1459,8 @@ const InitTracker = () => {
       const isIncursion = tipo === 'incursion';
       const shouldHideContentIncursion = isIncursion && !caraB && totalEnemies > 0;
       return (
-        <div className={`${posicion === 'abajo' ? 'top-0 left-0 right-0' : 'absolute bottom-0 left-0 right-0'} backface-hidden ${ caraB ? 'rotate-y-180' : '' }`} >
-          <div className={`p-0 rounded-lg border-2 shadow-md ${bgColor} ${borderColor} max-w-[90%] mx-auto sm:max-w-[220px]`} >
+        <div className={`${posicion === 'abajo' ? 'absolute top-0 left-0 right-0' : 'absolute bottom-0 left-0 right-0'} backface-hidden ${ caraB ? 'rotate-y-180' : '' }`} >
+          <div className={`p-0 rounded-lg border-2 shadow-md ${bgColor} ${borderColor} max-w-[90%] mx-auto sm:max-w-[140px]`} >
             {/* Título */}
             <div className="flex justify-center mb-1 text-white font-bold text-xs sm:text-sm text-center">
               {title}
@@ -1575,7 +1575,7 @@ const InitTracker = () => {
     };
   
     return (
-      <div className={`relative w-full perspective hover:scale-105`}>
+      <div className={`w-full perspective hover:scale-105`}>
         
         <div
           className={`relative w-full h-full transition-transform duration-700 transform-style preserve-3d ${
