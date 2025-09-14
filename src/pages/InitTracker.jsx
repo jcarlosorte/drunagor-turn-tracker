0524cd5a574aba52176ad00b7db810fb7ca1ab2e
jcarlosorte <jcarlosorte@ugr.es>
2025-09-15 00:56:05 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -809,10 +809,11 @@ const InitTracker = () => {
         let logMsg = "";
         let escudosReducidos = 0;
         let reducir_cal = 0;
-  
+        let daño = 0;
+        
         // 🔹 Daño
         if (config.daño > 0) {
-          let daño = config.daño * estado.count;
+          daño = config.daño * estado.count;
   
           if (daño > 0) {
             if (config.prevenir === "si" && escudosDisponibles > 0) {
