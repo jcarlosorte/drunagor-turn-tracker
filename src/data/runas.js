539bab31a1ce7bab6558a8d033388949ca01f732
jcarlosorte <jcarlosorte@ugr.es>
2025-09-28 22:41:38 +0200

    Update runas.js

--- src/data/runas.js
+++ src/data/runas.js
@@ -2,7 +2,9 @@ export const RUNAS = [
   { id: "runa1", cara: "A", posicion: "abajo", nombre: "Runa", accion: "Roba", numRunas: 1, tipo: "runa"  },
   { id: "runa1", cara: "B", posicion: "abajo", nombre: "Runa", accion: "Roba", numRunas: 2, tipo: "runa"  }
 ];
-
+export const RUNAS_F = [
+  { id: "runa2", cara: "B", posicion: "abajo", nombre: "Runa", accion: "Roba", numRunas: 2, tipo: "runa"  }
+];
 export const ASALTO = [
   { id: "asalto1", cara: "A", posicion: "abajo", nombre: "Asalto_1", accion: "Emerge", accion2: "Asalto", numRunas: 1, tipo: "asalto"  },
   { id: "asalto1", cara: "B", posicion: "abajo", nombre: "Asalto_1", accion: "Desciende", accion2: "Asalto", numRunas: 1, tipo: "asalto"  }
