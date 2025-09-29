f2899f2fc488951223845407ed1dac1bf9730bd2
jcarlosorte <jcarlosorte@ugr.es>
2025-09-29 13:40:04 +0200

    Update ModalEnemyCard.jsx

--- src/components/ModalEnemyCard.jsx
+++ src/components/ModalEnemyCard.jsx
@@ -85,16 +85,20 @@ export const ModalEnemyCard = ({ uuid, enemy, onClose, onDelete, onVidaChange, o
     if (valor === "X") {
       return count;
     }
-  
+
     if (typeof valor === "string" && valor.startsWith("X+")) {
       const extra = parseInt(valor.slice(2), 10); // ej: "X+3" → 3
-      if (extra === 0) {
-        // X+0 → usar count con máximo de 4
-        return Math.min(count, 4);
+      if (categoria != "esbirro"){
+        if (extra === 0) {
+          // X+0 → usar count con máximo de 4
+          return Math.min(count, 4);
+        }
+        if (count <= 1) return extra;      // 0 o 1 runa → 3
+        if (count === 2) return extra + 1; // 2 runas → 4
+        return extra + 1;                  // 3 o más → 4
       }
-      if (count <= 1) return extra;      // 0 o 1 runa → 3
-      if (count === 2) return extra + 1; // 2 runas → 4
-      return extra + 1;                  // 3 o más → 4
+    } else {
+      return extra + count;
     }
   
     return valor; // Si es un número directo o no aplicable
