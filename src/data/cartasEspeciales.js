dd888e12e7850f7ea17127142af0d7916a202ded
jcarlosorte <jcarlosorte@ugr.es>
2025-09-29 13:22:38 +0200

    Update cartasEspeciales.js

--- src/data/cartasEspeciales.js
+++ src/data/cartasEspeciales.js
@@ -208,7 +208,7 @@ export const CARTAS_JEFE = [
     runePosition: 'arriba',
     idJefe: 'undead_king_boss_1',
     capacidades: 'boss_5',
-    lista_capacidad: ["ROBA_2_noShow", "INVOCA", "skeleton_archer_esbirro2", "esbirro"]
+    lista_capacidad: ["ROBA_2_noShow", "INVOCA", "skeleton_archer_esbirro1", "esbirro"]
   },
   {
     id: 'boss_6',
@@ -226,7 +226,7 @@ export const CARTAS_JEFE = [
     runePosition: 'abajo',
     idJefe: 'undead_king_boss_2',
     capacidades: 'boss_7',
-    lista_capacidad: ["ROBA_2", "INVOCA", "skeleton_archer_esbirro1", "esbirro"]
+    lista_capacidad: ["ROBA_2", "INVOCA", "skeleton_archer_esbirro2", "esbirro"]
   },
   {
     id: 'boss_8',
