31417b9e78666cc45123afa1d5d287fdd8717574
jcarlosorte <jcarlosorte@ugr.es>
2025-10-04 02:58:38 +0200

    Update CommanderCard.jsx

--- src/components/CommanderCard.jsx
+++ src/components/CommanderCard.jsx
@@ -53,7 +53,7 @@ const CommanderCard = ({ carta }) => {
 
         <div
           onClick={() => setShowModal(true)}
-          className={`cursor-pointer relative w-full max-w-[140px] min-w-[128px] min-h-[128px] p-2 rounded-lg border-2 shadow-md hover:scale-105 transition
+          className={`cursor-pointer relative w-full max-w-[140px] min-w-[128px] min-h-[100px] sm:min-w-[10px] sm:min-h-[10px] p-2 rounded-lg border-2 shadow-md hover:scale-105 transition
             ${carta.highlight ? ringClass : borderClass}`}
         >
 
