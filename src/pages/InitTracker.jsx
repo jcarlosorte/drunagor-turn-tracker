cde772cb0a9f2aa43fde6dc6999594c2632f06f8
jcarlosorte <jcarlosorte@ugr.es>
2025-10-06 01:41:08 +0200

    Update InitTracker.jsx

--- src/pages/InitTracker.jsx
+++ src/pages/InitTracker.jsx
@@ -2641,16 +2641,18 @@ const InitTracker = () => {
                   d="M0,160 C360,240 1080,80 1440,160 L1440,320 L0,320 Z"
                   fill="rgba(0, 255, 150, 0.2)"
                 />
-                <path
-                  d="M50,0 
-                     C60,100 40,200 50,300 
-                     C60,400 40,500 50,600 
-                     C60,700 40,800 50,900"
-                  fill="none"
-                  stroke="rgba(0, 255, 150, 0.2)"
-                  stroke-width="10"
-                />
               </svg>
+              <svg class="river-waves2" viewBox="0 0 100 800" preserveAspectRatio="none">
+              <path
+                d="M50,0 
+                   C60,100 40,200 50,300 
+                   C60,400 40,500 50,600 
+                   C60,700 40,800 50,900"
+                fill="none"
+                stroke="rgba(0, 255, 150, 0.2)"
+                stroke-width="10"
+              />
+            </svg>
             </div>
 
 
