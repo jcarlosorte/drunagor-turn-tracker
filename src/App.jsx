e652dc30127ef30d25aa0923fa36165f8d0a4c13
jcarlosorte <jcarlosorte@ugr.es>
2025-10-06 23:25:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,31 @@
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import { useLanguage } from "@/context/LanguageContext";
 import AnimatedRoutes from "@/components/AnimatedRoutes";
-import LupaCursor from "@/assets/assets_logo/Lupa.png";
+import fondoDrunagor from "@/assets/PlPhfsK0-1-smaller.png
 
 function Header() {
   const { translations } = useLanguage();
+
   return (
-    <>
-      <img
-        src={`${import.meta.env.BASE_URL}Corebox_Logo-1536x864.png`}
-        alt="Drunagor Logo"
-        className="mx-auto mb-4 w-1/2 "
-      />
-      <h1 className="text-4xl font-bold mb-6">
-        {translations.home.title}
-      </h1>
-    </>
+    <div
+      className="relative w-full bg-cover bg-center bg-no-repeat py-8"
+      style={{ backgroundImage: `url(${fondoDrunagor})` }}
+    >
+      {/* Capa oscura opcional para mejorar contraste */}
+      <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+
+      {/* Contenido encima del fondo */}
+      <div className="relative z-10 text-center">
+        <img
+          src={`${import.meta.env.BASE_URL}Corebox_Logo-1536x864.png`}
+          alt="Drunagor Logo"
+          className="mx-auto mb-4 w-1/2"
+        />
+        <h1 className="text-4xl font-bold mb-6 text-white drop-shadow-md">
+          {translations.home.title}
+        </h1>
+      </div>
+    </div>
   );
 }
 
